0433e7bd1a31d42f0c7d0b61a75c2556bbcaac56
Adam Buchowski <adam.buchowski1@gmail.com>
2023-02-26 13:26:53 +0100

    Added and renamed event handlers

---
+++ Pages/InstallPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Windows.Navigation;
+using ModernWpf.Controls;
+
+namespace SpicetifyManager.Pages
+{
+    /// <summary>
+    /// Interaction logic for InstallPage.xaml
+    /// </summary>
+    public partial class InstallPage : Page
+    {
+        public InstallPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Spicetify_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://spicetify.app/docs/getting-started")
+            {
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+
+        private void Issues_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/AdotBdot/SpicetifyManager/issues")
+            {
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+    }
+}
